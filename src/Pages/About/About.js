b0fc167b7f5902ddf7c20c27d9d2aae864c2faa1
Al-aminhossain1 <md.al.aminhossain1785@gmail.com>
2022-04-27 21:34:26 +0600

    fake data loaded and show the site

---
+++ src/Pages/About/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>this is About us</h1>
+            <img src={'https://i.ibb.co/PhRdRP6/close-up-hands-unrecognizable-mechanic-doing-car-service-maintenance.jpg'} alt="" />
+
+            <img src={'https://i.ibb.co/S3HY27P/erik-mclean-3u-Hl-GFnz-PDU-unsplash-1-1.png'} alt="" />
+        </div>
+
+    );
+};
+
+export default About;
